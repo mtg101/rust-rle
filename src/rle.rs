f88c8041e262d908aab18e3b643ff0ae307f0e2a
Russell Bulmer <russell.bulmer@gmail.com>
2025-07-25 16:56:28 +0100

    checkpoint after starting print functions so I can set up on the laptop too

--- src/rle.rs
+++ src/rle.rs
@@ -2,19 +2,19 @@
 // takes vector of bytes
 // returns vector of tuples: byte, num_times
 // becasue z80 register is 8bit, num_times is 8bit, so $FE 500 in a row is $FE, 255 / $FE, 245 
-pub fn rle(raw: Vec<u8>) -> Vec<(u8, u8)> {
-    println!("Raw input bytes: {}", raw.len());     // len rather than length - already better than Java! :)
+pub fn rle(bitmap: Vec<u8>) -> Vec<(u8, u8)> {
+    println!("Bitmap input bytes: {}", bitmap.len());     // len rather than length - already better than Java! :)
     let mut rle_vec: Vec<(u8, u8)> = Vec::new();
 
     let mut index = 0;
 
-    while index < raw.len() {
-        let next_byte: u8 = raw[index];
+    while index < bitmap.len() {
+        let next_byte: u8 = bitmap[index];
         let mut count = 1;
 
         index += 1;
 
-        while index < raw.len() && next_byte == raw[index] {
+        while index < bitmap.len() && next_byte == bitmap[index] {
             count += 1;
             index += 1;
             if count == 255 {
@@ -25,10 +25,29 @@ pub fn rle(raw: Vec<u8>) -> Vec<(u8, u8)> {
         rle_vec.push((next_byte, count));        
     }
 
-    println!("RLE bytes: {}", rle_vec.len() * 2);     // len rather than length - already better than Java! :)
+    println!("RLE bytes: {}", rle_vec.len() * 2);     // *2 for tuple of bytes
     rle_vec       // yeah this is gonna feel weird for a while...
 }
 
+pub fn rle_print_bitmap_raw(bitmap: Vec<u8>) {
+
+}
+
+pub fn rle_print_bitmap_z80(bitmap: Vec<u8>) {
+
+}
+
+pub fn rle_print_rle_raw(rle_bytes:  Vec<(u8, u8)>) {
+
+}
+
+pub fn rle_print_rle_z80(rle_bytes:  Vec<(u8, u8)>) {
+
+}
+
+
+
+// tests in same file... also odd
 
 #[cfg(test)]              // commented out so 'build' builds the tests too
 mod tests {
@@ -38,43 +57,43 @@ mod tests {
         // tests empty returns empty 
         // (kinda pointless but it's a first test of anything -- objects vs values in them type issues)
         fn does_empty_rle() {                                 
-            let raw: Vec<u8> = Vec::new();
+            let bitmap: Vec<u8> = Vec::new();
             let rle_good: Vec<(u8, u8)> = Vec::new();
-            let rle_test: Vec<(u8, u8)> = rle(raw);
+            let rle_test: Vec<(u8, u8)> = rle(bitmap);
             assert_eq!(rle_good, rle_test);
         }
 
     #[test]
         // tests something doesn't return empty - a negative
         fn does_something_not_empty_rle() {                                 
-            let raw: Vec<u8> = Vec::new();
+            let bitmap: Vec<u8> = Vec::new();
             let rle_good: Vec<(u8, u8)> = vec![(1, 2)];
-            let rle_test: Vec<(u8, u8)> = rle(raw);
+            let rle_test: Vec<(u8, u8)> = rle(bitmap);
             assert_ne!(rle_good, rle_test);
         }
 
     #[test]
         // doesn't worry about breaching u8 for repeated times
         fn does_small_rle() {                                 
-            let raw: Vec<u8> = vec![1, 2, 2, 3, 3, 3, 4, 4, 3, 2, 1];
+            let bitmap: Vec<u8> = vec![1, 2, 2, 3, 3, 3, 4, 4, 3, 2, 1];
             let rle_good: Vec<(u8, u8)> = vec![(1, 1), (2, 2), (3, 3), (4, 2), (3,1), (2, 1), (1, 1)];
-            let rle_test: Vec<(u8, u8)> = rle(raw);
+            let rle_test: Vec<(u8, u8)> = rle(bitmap);
             assert_eq!(rle_good, rle_test);
         }
 
     #[test]
         // more than 255 in a row, so for $FE 500 times expect back $FE, 255 / $FE, 245
         fn does_large_rle() {
-            let mut raw: Vec<u8> = Vec::new();
-            raw.push(1);
+            let mut bitmap: Vec<u8> = Vec::new();
+            bitmap.push(1);
 
             for _number in 0..500 {
-                raw.push(2);
+                bitmap.push(2);
             }
-            raw.push(1);
+            bitmap.push(1);
 
             let rle_good: Vec<(u8, u8)> = vec![(1, 1), (2, 255), (2, 245), (1, 1)];
-            let rle_test: Vec<(u8, u8)> = rle(raw);
+            let rle_test: Vec<(u8, u8)> = rle(bitmap);
 
             assert_eq!(rle_good, rle_test);
         }
